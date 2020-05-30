8c61d2864516bba936a8dc909eeb3ca97a1e7b30
BokuanYang <davidyang19971209@gmail.com>
2020-05-30 19:03:24 +0800

    Question 20

---
+++ 栈/src/有效的括号20/Demo.java
@@ -0,0 +1,16 @@
+package ÓÐÐ§µÄÀ¨ºÅ20;
+
+import java.util.Stack;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	Solution s = new Solution();
+	System.out.println(s.isValid("()"));
+	}
+	
+
+
+}
