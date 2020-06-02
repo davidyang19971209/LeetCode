f7e89187a232ab445cc96d5cf10c36a4388503a8
BokuanYang <davidyang19971209@gmail.com>
2020-06-02 15:05:25 +0800

    同155

---
+++ 栈/src/包含Min函数的栈/Demo.java
@@ -0,0 +1,18 @@
+package °üº¬Minº¯ÊýµÄÕ»;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MinStack minStack = new MinStack();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+		minStack.min();  
+		minStack.pop();
+		minStack.top();     
+		minStack.min();  
+
+	}
+
+}
---
+++ 栈/src/包含Min函数的栈/MinStack.java
@@ -0,0 +1,38 @@
+package °üº¬Minº¯ÊýµÄÕ»;
+
+import java.util.Stack;
+
+public class MinStack {
+	
+	private Stack<Integer> s1 = new Stack<>();
+	private Stack<Integer> s2 = new Stack<>();
+	
+    public MinStack() {
+
+    }
+    
+    public void push(int x) {
+    	s1.push(x);
+    	if(s2.isEmpty()||(!s2.isEmpty()&&s2.peek()>=x))
+    	{
+    		s2.push(x);
+    	}
+
+    }
+    
+    public void pop() {
+    	int temp = s1.pop();
+    	
+    	if(temp == s2.peek())
+    		s2.pop();
+    }
+    
+    public int top() {
+    	return s1.peek();
+    }
+    
+    public int min() {
+    	return s2.peek();
+    }
+
+}
