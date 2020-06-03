4ceb330e694b806fd4744a0c3dfbe04b204cf8b0
BokuanYang <davidyang19971209@gmail.com>
2020-06-03 15:00:19 +0800

    Update Solution3.java

--- 栈_medium/src/二叉树中序遍历94/Solution3.java
+++ 栈_medium/src/二叉树中序遍历94/Solution3.java
@@ -1,5 +1,35 @@
 package ¶þ²æÊ÷ÖÐÐò±éÀú94;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution3 {
+	public List < Integer > inorderTraversal(TreeNode root) 
+	{
+		List<Integer> res = new ArrayList<>();
+		TreeNode curr = root;
+		TreeNode pre;
+		
+		while(curr!=null)
+		{
+			if(curr.left==null)
+			{
+				res.add(curr.val);
+				curr = curr.right;
+			}
+			else
+			{
+				pre = curr.left;
+				curr = curr.right;
+			}
+			pre.right=cur;
+			TreeNode temp = curr;
+			curr = curr.left;
+			temp.left = null;
+		}
+		
+		return res;
+		
+	}
 
 }
