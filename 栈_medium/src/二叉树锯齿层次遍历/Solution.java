9394ab95daf25aa09c0b32a341019304da426d7e
BokuanYang <davidyang19971209@gmail.com>
2020-06-03 15:58:57 +0800

    广度优先算法

---
+++ 栈_medium/src/二叉树锯齿层次遍历/Solution.java
@@ -0,0 +1,56 @@
+package ¶þ²æÊ÷¾â³Ý²ã´Î±éÀú;
+
+public class Solution {
+	
+ public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+	    if (root == null) {
+	      return new ArrayList<List<Integer>>();
+	    }
+
+	    List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+	    LinkedList<TreeNode> node_deque = new LinkedList<>();
+
+	    node_deque.addLast(root);
+	    node_deque.addLast(null);
+
+	    LinkedList<Integer> level_list = new LinkedList<>();
+	    boolean is_order_left = true;
+
+	    while(node_deque.size()>0)
+	    {
+	        TreeNode current_node = node_deque.pollFirst();
+	        if(current_node!=null)
+	        {
+	            if(is_order_left)
+	            {
+	                level_list.addLast(current_node.val);
+	            }
+	            else
+	            {
+	                level_list.addFirst(current_node.val);
+	            }
+	            if(current_node.left!=null)
+	            {
+	                node_deque.addLast(current_node.left);
+	            }
+	            if(current_node.right!=null)
+	            {
+	                node_deque.addLast(current_node.right);
+	            }
+	        }
+	        else
+	        {
+	            results.add(level_list);
+	            level_list = new LinkedList<Integer>();
+
+	            if(node_deque.size()>0)
+	            {
+	                node_deque.addLast(null);
+	            }
+	            is_order_left = !is_order_left;
+	        }
+	    }
+	    return results;
+	  }
+}
